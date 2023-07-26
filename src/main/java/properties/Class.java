f998eb54a645ac493ba966de49bc8ad42459a6cb
Timothy Buchak <timothy.buchak@gmail.com>
2023-07-26 16:58:41 -0400

    Copying from project

---
+++ src/main/java/properties/Class.java
@@ -0,0 +1,53 @@
+package properties;
+
+public class Class {
+
+    private int str;
+    private int dex;
+    private int con;
+    private int intel;
+    private int wis;
+
+    public Class (){
+
+    }
+    public int getStr() {
+        return str;
+    }
+
+    public int getDex() {
+        return dex;
+    }
+
+    public int getCon() {
+        return con;
+    }
+
+    public int getIntel() {
+        return intel;
+    }
+
+    public int getWis() {
+        return wis;
+    }
+
+    public void setStr(int str) {
+        this.str = str;
+    }
+
+    public void setDex(int dex) {
+        this.dex = dex;
+    }
+
+    public void setCon(int con) {
+        this.con = con;
+    }
+
+    public void setIntel(int intel) {
+        this.intel = intel;
+    }
+
+    public void setWis(int wis) {
+        this.wis = wis;
+    }
+}
---
+++ src/main/java/ui/Generator.java
@@ -0,0 +1,12 @@
+package ui;
+
+import properties.Fighter;
+
+public class Generator {
+
+    public static void main(String[] args) {
+        System.out.println("Choose a class: (F)ighter, (R)andom");
+        Fighter testChar = new Fighter();
+
+    }
+}
