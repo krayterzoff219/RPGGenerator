3710f7d710bb278edde7a4691c68c24e8a454ecb
Timothy Buchak <timothy.buchak@gmail.com>
2023-07-27 04:44:46 -0400

    Added class variables and toString method

--- src/main/java/properties/Class.java
+++ src/main/java/properties/Class.java
@@ -7,9 +7,21 @@ public class Class {
     private int con;
     private int intel;
     private int wis;
+    private int cha;
+    private int hitPoints;
 
     public Class (){
 
+    }
+
+    public Class (int str, int dex, int con, int intel, int wis, int cha, int hitPoints){
+        this.str = str;
+        this.dex = dex;
+        this.con = con;
+        this.intel = intel;
+        this.wis = wis;
+        this.cha = cha;
+        this.hitPoints = hitPoints;
     }
     public int getStr() {
         return str;
@@ -31,6 +43,14 @@ public class Class {
         return wis;
     }
 
+    public int getCha() {
+        return cha;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
     public void setStr(int str) {
         this.str = str;
     }
@@ -49,5 +69,17 @@ public class Class {
 
     public void setWis(int wis) {
         this.wis = wis;
+    }
+
+    public void setCha(int cha) {
+        this.cha = cha;
+    }
+
+    public void setHitPoints(int hitPoints){
+        this.hitPoints = hitPoints;
+    }
+
+    public String toString(){
+        return "STR: " + str + "  DEX: " + dex + "  CON: " + con + "  INT: " + intel + "  WIS: " + wis + "  CHA: " + cha + "  HP: " + hitPoints;
     }
 }
---
+++ src/test/java/properties/ClassTest.java
@@ -0,0 +1,12 @@
+package properties;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ClassTest {
+
+    @Test
+    public void testToString() {
+    }
+}
